4911085ec84529bde3124b12596f6d894996fc5c
Tony37594 <137598141+Tony37594@users.noreply.github.com>
2026-01-09 19:56:08 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ ENTRY_NAME = "entry.635424914"
 ENTRY_LOC = "entry.1499233920" 
 
 st.markdown("<h1 style='text-align: center;'>🚗 Dad's Car Share</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Parked at Flyaway 0800 77 66 99</h1>", unsafe_allow_html=True)
 st.title("Parked at Flyaway 0800 77 66 99")
 
 # --- 1. UPDATE LOCATION ---
